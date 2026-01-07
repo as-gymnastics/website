78e71f9de191a82996b0eacf53478ce4eddf9346
Stefan Ovezea <sovezea@gmail.com>
2026-01-07 16:14:15 +0200

    fix

--- src/app/(frontend)/[slug]/page.tsx
+++ src/app/(frontend)/[slug]/page.tsx
@@ -58,7 +58,7 @@ export default async function Page({ params: paramsPromise }: Args) {
     return <PayloadRedirects url={url} />
   }
 
-  const { hero, layout } = page
+  const { hero } = page
 
   return (
     <article className="pt-16 pb-24">
@@ -69,7 +69,6 @@ export default async function Page({ params: paramsPromise }: Args) {
       {draft && <LivePreviewListener />}
 
       <RenderHero {...hero} />
-      <RenderBlocks blocks={layout} />
     </article>
   )
 }
